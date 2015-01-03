3e5faa8863efa14b44f1ef852195ee86234756ec
Akku Iki Alie <ali@firzil.co.id>
2015-01-03 10:46:59 +0700

    Constants : add session_admin_name

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,7 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+define('SESSION_NAME_ADMIN', 					'SIPRAM_ADMIN');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
